e5797970e980c5c5d5585f180934b7f856962a73
Guilherme de Oliveira <guilherme.061@gmail.com>
2022-11-08 20:38:37 -0800

    more renaming

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #define GUEEPO2D_MAIN
 #include <gueepo2d.h>
 
-class Sample : public gueepo::Application {
+class BardRoguelike : public gueepo::Application {
 public:
-	Sample() : Application("sample ", 640, 360) {}
-	~Sample() {}
+	BardRoguelike() : Application("bardRL ", 640, 360) {}
+	~BardRoguelike() {}
 };
 
 gueepo::Application* gueepo::CreateApplication() {
-	return new Sample();
+	return new BardRoguelike();
 }
